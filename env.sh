cc94bf6ee454eaa1759242821c12ce96548586dc
Spring MC <Heresy.MC@gmail.com>
2017-07-08 10:08:10 +0800

    What: new env.sh for both development and test envs
    Why:
    
      * setup golang path
    
    How:
    
      * source env.sh

---
+++ env.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+if [ "$APPROOT" = "" ]; then
+    export APPROOT=$(pwd)
+fi
+
+# adjust GOPATH
+case ":$GOPATH:" in
+    *":$APPROOT:"*) :;;
+    *) GOPATH=$APPROOT:$GOPATH;;
+esac
+export GOPATH
+
+
+# adjust PATH
+if [ -n "$ZSH_VERSION" ]; then
+    readopts="rA"
+else
+    readopts="ra"
+fi
+while IFS=':' read -$readopts ARR; do
+    for i in "${ARR[@]}"; do
+        case ":$PATH:" in
+            *":$i/bin:"*) :;;
+            *) PATH=$i/bin:$PATH
+        esac
+    done
+done <<< "$GOPATH"
+export PATH
+
+
+# mock development && test envs
+if [ -d "$APPROOT/ifaces" ]; then
+    if [ ! -d "$APPROOT/src/github.com/kirk-enterprise/openstack" ]; then
+        mkdir -p "$APPROOT/src/github.com/kirk-enterprise"
+
+        if [ "$1" == "travis" ]; then
+            cp -r "$APPROOT" "$APPROOT/src/github.com/kirk-enterprise"
+        else
+            ln -s "$APPROOT" "$APPROOT/src/github.com/kirk-enterprise"
+        fi
+    fi
+fi
