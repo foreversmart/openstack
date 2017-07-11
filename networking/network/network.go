55ae68a1e7525af4ff6f70132e481ab780e7e642
csxuejin <602296435@qq.com>
2017-07-11 18:03:59 +0800

    add function for network

--- networking/network/network.go
+++ networking/network/network.go
@@ -3,12 +3,11 @@ package network
 import (
 	"net/url"
 
-	"github.com/kirk-enterprise/openstack/lib/ifaces"
-	"github.com/kirk-enterprise/openstack/lib/models"
-	"github.com/kirk-enterprise/openstack/lib/options"
+	"github.com/kirk-enterprise/openstack-golang-sdk/lib/ifaces"
+	"github.com/kirk-enterprise/openstack-golang-sdk/lib/models"
+	"github.com/kirk-enterprise/openstack-golang-sdk/lib/options"
 	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/openstack/networking/v2/networks"
-	"github.com/rackspace/gophercloud/openstack/networking/v2/subnets"
 )
 
 type Network struct {
@@ -23,17 +22,10 @@ func New(client ifaces.Openstacker) *Network {
 	}
 }
 
-func (n *Network) CreateNetwork(name, tenantId string, shared, adminStateUp bool) (network *models.NetworkModel, err error) {
+func (n *Network) Create(opts *networks.CreateOpts) (network *models.NetworkModel, err error) {
 	client, err := n.Client.NetworkClient()
 	if err != nil {
 		return network, err
-	}
-
-	opts := networks.CreateOpts{
-		Name:         name,
-		AdminStateUp: &adminStateUp,
-		Shared:       &shared,
-		TenantID:     tenantId,
 	}
 
 	result := networks.Create(client, opts)
@@ -57,22 +49,21 @@ func (n *Network) All() (infos []*models.NetworkModel, err error) {
 	}
 
 	return models.ExtractNetworksByPage(page)
-
 }
 
-func (n *Network) AllByParams(param *options.NetworkQueryOpt) (infos []*models.NetworkModel, err error) {
+func (n *Network) AllByParams(opts *options.NetworkQueryOpt) (infos []*models.NetworkModel, err error) {
 	client, err := n.Client.NetworkClient()
 	if err != nil {
 		return
 	}
 
-	opts := url.Values{}
-	if param.AllTenants != "" {
-		opts.Add("all_tenants", param.AllTenants)
+	params := url.Values{}
+	if opts.AllTenants != "" {
+		params.Add("all_tenants", opts.AllTenants)
 	}
 
 	var result gophercloud.Result
-	_, result.Err = client.Get(client.ServiceURL("networks")+"?"+opts.Encode(), &result.Body, &gophercloud.RequestOpts{
+	_, result.Err = client.Get(client.ServiceURL("networks")+"?"+params.Encode(), &result.Body, &gophercloud.RequestOpts{
 		OkCodes: []int{200, 201},
 	})
 	if result.Err != nil {
@@ -83,49 +74,7 @@ func (n *Network) AllByParams(param *options.NetworkQueryOpt) (infos []*models.N
 	return models.ExtractNetworks(result)
 }
 
-func (n *Network) CreateSubnet(name, networkId, cidr, tenantId string, ipVersion int) (subnet *models.SubnetModel, err error) {
-	client, err := n.Client.NetworkClient()
-	if err != nil {
-		return subnet, err
-	}
-
-	opts := subnets.CreateOpts{
-		Name:      name,
-		NetworkID: networkId,
-		CIDR:      cidr,
-		TenantID:  tenantId,
-		IPVersion: ipVersion,
-	}
-
-	result := subnets.Create(client, opts)
-	return models.ExtractSubnetByResult(result.Result)
-}
-
-func (n *Network) AllSubnets() (subnetInfos []*models.SubnetModel, err error) {
-	client, err := n.Client.NetworkClient()
-	if err != nil {
-		return subnetInfos, err
-	}
-
-	subnetInfos = make([]*models.SubnetModel, 0, 5)
-
-	opts := subnets.ListOpts{
-		TenantID: n.Client.ProjectID(),
-	}
-
-	page, err := subnets.List(client, opts).AllPages()
-	if err != nil {
-		return
-	}
-	subnetInfos, err = models.ExtractSubnetsByPage(page)
-	if err != nil {
-		return
-	}
-
-	return subnetInfos, err
-}
-
-func (n *Network) Query(id string) (info *models.NetworkModel, err error) {
+func (n *Network) Show(id string) (info *models.NetworkModel, err error) {
 	// find the network in the admin created networks
 	networkInfos := make([]*models.NetworkModel, 0)
 	for _, value := range networkInfos {
@@ -147,16 +96,7 @@ func (n *Network) Query(id string) (info *models.NetworkModel, err error) {
 	return
 }
 
-func (n *Network) AllSubnetsByID(networkId string) (subnetIds []string, err error) {
-	networkInfo, err := n.Query(networkId)
-	if err != nil || networkInfo == nil {
-		return
-	}
-
-	return networkInfo.Subnets, err
-}
-
-func (n *Network) DeleteNetwork(networkId string) error {
+func (n *Network) Delete(networkId string) error {
 	client, err := n.Client.NetworkClient()
 	if err != nil {
 		return err
