500da02ad6badf6cb22eded4a7d561cabf3eeb1a
csxuejin <602296435@qq.com>
2017-07-11 22:22:13 +0800

    add ToQuery() function

--- lib/options/network.go
+++ lib/options/network.go
@@ -1,6 +1,25 @@
 package options
 
+import "net/url"
+
 type NetworkQueryOpt struct {
-	AllTenants string
-	ProjectId  string
+	AllTenants *string `json:"all_tenants"`
+	ProjectId  *string `json:"project_id"`
+}
+
+func (opts *NetworkQueryOpt) ToQuery() (options url.Values) {
+	options = url.Values{}
+	if opts == nil {
+		return
+	}
+
+	if opts.AllTenants != nil {
+		options.Add("all_tenants", *opts.AllTenants)
+	}
+
+	if opts.ProjectId != nil {
+		options.Add("project_id", *opts.ProjectId)
+	}
+
+	return
 }
