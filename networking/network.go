312f83cd261794ad4c3deac7d268702f5e27b112
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-07-14 15:51:14 +0800

    add netwoking test case

--- networking/network.go
+++ networking/network.go
@@ -1,1 +1,22 @@
 package networking
+
+import (
+	"github.com/kirk-enterprise/openstack-golang-sdk/lib/ifaces"
+	"github.com/kirk-enterprise/openstack-golang-sdk/networking/floatingip"
+)
+
+type Networking struct {
+	client ifaces.Openstacker
+
+	_ bool
+}
+
+func New(client ifaces.Openstacker) *Networking {
+	return &Networking{
+		client: client,
+	}
+}
+
+func (n *Networking) NewFloatingIPer() ifaces.FloatingIPer {
+	return floatingip.New(n.client)
+}
--- networking/network_test.go
+++ networking/network_test.go
@@ -1,1 +1,16 @@
 package networking
+
+import (
+	"testing"
+
+	"github.com/golib/assert"
+	"github.com/kirk-enterprise/openstack-golang-sdk/lib/ifaces"
+)
+
+func Test_Networking(t *testing.T) {
+	assertion := assert.New(t)
+
+	n := New(nil)
+	assertion.NotNil(n)
+	assertion.Implements((*ifaces.FloatingIPer)(nil), n.NewFloatingIPer())
+}
