7be54fdf15e2558dd84c665814a354979409b62a
Jing Wang <wangjing@qiniu.com>
2017-07-14 18:04:09 +0800

    import qbox package

--- networking/secgroup/secgroup_test.go
+++ networking/secgroup/secgroup_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/golib/assert"
-	"github.com/kirk-enterprise/openstack-golang-sdk/lib/options"
+	"github.com/qbox/openstack-golang-sdk/lib/options"
 )
 
 func Test_Create_SecurityGroup(t *testing.T) {
