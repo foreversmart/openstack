5eb4a18a6dda92e68ab9eba2a9277f5c88a852c9
csxuejin <602296435@qq.com>
2017-07-18 10:37:46 +0800

    ToQuery()

--- lib/options/secrule.go
+++ lib/options/secrule.go
@@ -1,5 +1,10 @@
 package options
 
+import (
+	"net/url"
+	"strconv"
+)
+
 type CreateSecruleOpts struct {
 	RemoteGroupID  *string `json:"remote_group_id"`
 	Direction      *string `json:"direction"`
@@ -43,4 +48,60 @@ type ListSecRuleOpts struct {
 
 func (opts *ListSecRuleOpts) IsValid() bool {
 	return true
+}
+
+func (opts *ListSecRuleOpts) ToQuery() url.Values {
+	options := url.Values{}
+
+	if opts != nil {
+		if opts.ID != nil {
+			options.Add("id", *opts.ID)
+		}
+
+		if opts.RemoteGroupID != nil {
+			options.Add("remote_group_id", *opts.RemoteGroupID)
+		}
+
+		if opts.Direction != nil {
+			options.Add("direction", *opts.Direction)
+		}
+
+		if opts.Protocol != nil {
+			options.Add("protocol", *opts.Protocol)
+		}
+
+		if opts.EtherType != nil {
+			options.Add("ethertype", *opts.EtherType)
+		}
+
+		if opts.PortRangeMin != nil {
+			options.Add("port_range_min", strconv.Itoa(*opts.PortRangeMin))
+		}
+
+		if opts.PortRangeMax != nil {
+			options.Add("port_range_max", strconv.Itoa(*opts.PortRangeMax))
+		}
+
+		if opts.SecGroupID != nil {
+			options.Add("security_group_id", *opts.SecGroupID)
+		}
+
+		if opts.TenantID != nil {
+			options.Add("tenant_id", *opts.TenantID)
+		}
+
+		if opts.ProjectID != nil {
+			options.Add("project_id", *opts.ProjectID)
+		}
+
+		if opts.RemoteIPPrefix != nil {
+			options.Add("remote_ip_prefix", *opts.RemoteIPPrefix)
+		}
+
+		if opts.Description != nil {
+			options.Add("description", *opts.Description)
+		}
+	}
+
+	return options
 }
