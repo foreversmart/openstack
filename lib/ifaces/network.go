6c7b55f4195f48e8decc25f71ea3b6b1605ebed0
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-07-18 14:52:08 +0800

    fix network ifaces

--- lib/ifaces/network.go
+++ lib/ifaces/network.go
@@ -3,11 +3,10 @@ package ifaces
 import (
 	"github.com/qbox/openstack-golang-sdk/lib/models"
 	"github.com/qbox/openstack-golang-sdk/lib/options"
-	"github.com/rackspace/gophercloud/openstack/networking/v2/networks"
 )
 
 type Networker interface {
-	Create(opts *networks.CreateOpts) (network *models.NetworkModel, err error)
+	Create(opts *options.CreateNetworkOpts) (network *models.NetworkModel, err error)
 	All() (networks []*models.NetworkModel, err error)
 	AllByParams(opts *options.ListNetworkOpt) (networks []*models.NetworkModel, err error)
 	Show(id string) (network *models.NetworkModel, err error)
