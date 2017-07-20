d8d834d9f529a5ed064b19032aa770f96cca0ba4
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-07-20 11:28:47 +0800

    split server interface

--- lib/ifaces/servers.go
+++ lib/ifaces/servers.go
@@ -5,34 +5,47 @@ import (
 	"github.com/qbox/openstack-golang-sdk/lib/options"
 )
 
-type Serverser interface {
+type Server interface {
 	Create(opts options.CreateServersOpts) (server *models.ServersModel, err error)
 	All() (servers []*models.ServersModel, err error)
 	AllByParams(opts *options.ListServersOpts) (servers []*models.ServersModel, err error)
 	Show(id string) (server *models.ServersModel, err error)
-	SearchByFixedIP(ip string) ([]*models.ServersModel, error)
 	Update(id string, opts options.UpdateServersOpts) (server *models.ServersModel, err error)
 	Delete(id string) error
-
-	ServerActions
 }
 
-type ServerActions interface {
-	ChangeAdminPassword(id, newPassword string) error                    // 重置系统管理员帐号密码
-	AdminPassword(id string) (passwd string, err error)                  // 查询系统管理员帐号密码
-	Start(id string) error                                               // 开机
-	Reboot(id string) error                                              // 重启
-	Shutdown(id string) error                                            // 软关闭
-	Stop(id string) error                                                // 强制关闭
-	CreateSnapshot(id, name string) (snapshotID string, err error)       // 创建主机快照
-	BindPort(id, portID string) error                                    // 绑定网卡
-	UnbindPort(id, portID string) error                                  // 解绑网卡
-	Ports(id string) ([]string, error)                                   // 获取虚拟机的网卡
-	BindKeys(id string, keys []string) error                             // 绑定key
-	UnbindKeys(id string, keys []string) error                           // 解绑key
-	MountVolume(id, volumeID string) error                               // 挂载磁盘
-	UnmountVolume(id, volumeID string) error                             // 卸载磁盘
-	ModifyFlavor(id, flavorID string) error                              // 修改主机配置
-	Rebuild(id, imageID string) (server *models.ServersModel, err error) // 重置主机
-	Vnc(id string) (string, error)                                       // vnc link
+type ServerManager interface {
+	ChangeAdminPassword(serverID, newPassword string) error                    // 重置系统管理员帐号密码
+	AdminPassword(serverID string) (passwd string, err error)                  // 查询系统管理员帐号密码
+	Start(serverID string) error                                               // 开机
+	Stop(serverID string) error                                                // 强制关闭
+	Reboot(serverID string) error                                              // 重启
+	Shutdown(serverID string) error                                            // 软关闭
+	Rebuild(serverID, imageID string) (server *models.ServersModel, err error) // 重置主机
+	Resize(serverID, flavorID string) error                                    // 修改主机配置
+	Vnc(serverID string) (url string, err error)                               // vnc link
+
+	SearchByFixedIP(ip string) (servers []*models.ServersModel, err error)
+}
+
+type ServerPorter interface {
+	All(serverID string) (ports *[]models.PortModel, err error) // 获取虚拟机的网卡
+	Bind(serverID, portID string) error                         // 绑定网卡
+	Unbind(serverID, portID string) error                       // 解绑网卡
+}
+
+type ServerKeyer interface {
+	All(serverID string) (keypairs *[]models.KeypairModel, err error)
+	Bind(id string, keys []string) error   // 绑定key
+	Unbind(id string, keys []string) error // 解绑key
+}
+
+type ServerVolumer interface {
+	All(serverID string) (volumes *[]models.VolumeModel, err error)
+	MountVolume(id, volumeID string) error   // 挂载磁盘
+	UnmountVolume(id, volumeID string) error // 卸载磁盘
+}
+
+type ServerImager interface {
+	Create(serverID, imageName string) (image *models.ImageModel, err error) // 创建主机快照
 }
