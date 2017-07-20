9bdf79e7b1ae44b76e28eac1d36d5928eb3d78fa
csxuejin <602296435@qq.com>
2017-07-20 13:06:47 +0800

    modify model

--- lib/models/secgroup.go
+++ lib/models/secgroup.go
@@ -6,29 +6,12 @@ import (
 )
 
 type SecurityGroupModel struct {
-	ID          string `mapstructure:"id" json:"id"`
-	TenantID    string `mapstructure:"tenant_id" json:"tenant_id"`
-	ProjectID   string `mapstructure:"project_id" json:"project_id"`
-	Name        string `mapstructure:"name" json:"name"`
-	Description string `mapstructure:"description" json:"description"`
-	// Rules       []*SecurityRuleModel `json:"security_group_rules" mapstructure:"security_group_rules"`"
-}
-
-func ExtractSecurityGroups(result gophercloud.Result) (securitygroups []*SecurityGroupModel, err error) {
-	if result.Err != nil {
-		return nil, result.Err
-	}
-
-	var response struct {
-		SecurityGroups []*SecurityGroupModel `mapstructure:"security_groups"`
-	}
-
-	err = mapstructure.Decode(result.Body, &response)
-	if err == nil {
-		securitygroups = response.SecurityGroups
-	}
-
-	return
+	ID          string               `mapstructure:"id" json:"id"`
+	TenantID    string               `mapstructure:"tenant_id" json:"tenant_id"`
+	ProjectID   string               `mapstructure:"project_id" json:"project_id"`
+	Name        string               `mapstructure:"name" json:"name"`
+	Description string               `mapstructure:"description" json:"description"`
+	Rules       []*SecGroupRuleModel `json:"security_group_rules" mapstructure:"security_group_rules"`
 }
 
 func ExtractSecurityGroup(result gophercloud.Result) (securitygroup *SecurityGroupModel, err error) {
@@ -43,6 +26,27 @@ func ExtractSecurityGroup(result gophercloud.Result) (securitygroup *SecurityGro
 	err = mapstructure.Decode(result.Body, &response)
 	if err == nil {
 		securitygroup = response.SecurityGroup
+	}
+
+	return
+}
+
+func ExtractSecurityGroups(r gophercloud.Result) (securitygroups []*SecurityGroupModel, err error) {
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	return ExtractSecurityGroupsByBody(r.Body)
+}
+
+func ExtractSecurityGroupsByBody(body interface{}) (securitygroups []*SecurityGroupModel, err error) {
+	var resp struct {
+		SecurityGroups []*SecurityGroupModel `mapstructure:"security_groups"`
+	}
+
+	err = mapstructure.Decode(body, &resp)
+	if err == nil {
+		securitygroups = resp.SecurityGroups
 	}
 
 	return
