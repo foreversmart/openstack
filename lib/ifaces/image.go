f9e5c9641d8c24ec029cfcd7a2b38b2e41334950
rorschach <kuangyongqiao@qiniu.com>
2017-08-10 16:09:58 +0800

    add image.AllByParams hasNext params

--- lib/ifaces/image.go
+++ lib/ifaces/image.go
@@ -10,7 +10,7 @@ import (
 type Imager interface {
 	Create(opts *options.CreateImagesOpts) (image *models.ImageModel, err error)
 	All() (images []*models.ImageModel, err error)
-	AllByParams(opts *options.ListImagesOpts) (imageModels []*models.ImageModel, err error)
+	AllByParams(opts *options.ListImagesOpts) (imageModels []*models.ImageModel, hasNext bool, err error)
 	Show(id string) (image *models.ImageModel, err error)
 	Update(id string, opts *options.UpdateImagesOpts) (imageModel *models.ImageModel, err error)
 	Delete(id string) error
